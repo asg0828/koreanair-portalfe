40d49f49b0d7c1195d2ed068ecf83c635cf0d175
ahnsookyun <sk-ahn@kb-sys.co.kr>
2023-10-12 15:06:46 +0900

    feature: 워터마크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Login from './pages/Login';
+import Watermark from '@uiw/react-watermark';
 import { Providers } from '@components/ui';
 import '@/assets/styles/global.scss';
 
 const App = () => {
   return (
-    <Providers>
-      <Login />
-    </Providers>
+    <Watermark
+      content="워터마크"
+    >
+      <Providers>
+        <Login />
+      </Providers>
+    </Watermark>
   )
 };
 export default App;
