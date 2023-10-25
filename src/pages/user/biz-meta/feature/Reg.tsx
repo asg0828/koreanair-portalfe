e2c1e99149de723fcd96800c7cefc58cbcc406f0
kjm0810 <kjm980810@gmail.com>
2023-10-25 16:44:39 +0900

    style: biz-meta>feature 병합

--- src/pages/user/biz-meta/feature/Reg.tsx
+++ src/pages/user/biz-meta/feature/Reg.tsx
@@ -18,39 +18,41 @@ const Reg = () => {
         <Typography variant="h3">기본 정보</Typography>
         <HorizontalTable>
           <TR>
-            <TH required>대구분</TH>
-            <TD>
+            <TH required colSpan={1} align='right'>대구분</TH>
+            <TD colSpan={2}>
               <Select appearance="Outline" placeholder="전체" className="width-100">
                 <SelectOption value={1}>테스트</SelectOption>
               </Select>
             </TD>
-            <TH required>중구분</TH>
-            <TD>
+            <TH required colSpan={1} align='right'>중구분</TH>
+            <TD colSpan={2}>
               <Select appearance="Outline" placeholder="전체" className="width-100">
                 <SelectOption value={1}>테스트</SelectOption>
               </Select>
             </TD>
           </TR>
           <TR>
-            <TH>Feature ID</TH>
-            <TD>
-              <TextField className="width-100" />
+            <TH colSpan={1} align='right'>Feature ID</TH>
+            <TD colSpan={2}>
+              <TextField className="width-100" readOnly value={"12345"}/>
             </TD>
-            <TH required>Feature 타입</TH>
-            <TD>
-            <TextField className="width-100" />
+            <TH required colSpan={1} align='right'>Feature 타입</TH>
+            <TD colSpan={2}>
+              <Select appearance="Outline" placeholder="전체" className="width-100">
+                <SelectOption value={1}>테스트</SelectOption>
+              </Select>
             </TD>
           </TR>
           <TR>
-            <TH required>한글명</TH>
-            <TD>
+            <TH required colSpan={1} align='right'>한글명</TH>
+            <TD colSpan={2}>
               <Stack gap="SM" className="width-100">
                 <TextField className="width-100" />
                 <Button>중복확인</Button>
               </Stack>
             </TD>
-            <TH required>영문명</TH>
-            <TD>
+            <TH required colSpan={1} align='right'>영문명</TH>
+            <TD colSpan={2}>
               <Stack gap="SM" className="width-100">
                 <TextField className="width-100" />
                 <Button>중복확인</Button>
@@ -58,29 +60,29 @@ const Reg = () => {
             </TD>
           </TR>
           <TR>
-            <TH colSpan={1} required>Feature 정의</TH> 
-            <TD colSpan={3}>
-              <TextField className="width-100" />
+            <TH colSpan={1} required align='right'>Feature 정의</TH> 
+            <TD colSpan={5.01}>
+              <TextField className="width-100" multiline/>
             </TD>
           </TR>
           <TR>
-            <TH colSpan={1}>산출단위</TH>
-            <TD colSpan={3}>
+            <TH colSpan={1} align='right'>산출단위</TH>
+            <TD colSpan={5.01}>
               <Select appearance="Outline" placeholder="전체" className="width-100">
                 <SelectOption value={1}>테스트</SelectOption>
               </Select>
             </TD>
           </TR>
           <TR>
-            <TH colSpan={1}>산출로직</TH>
-            <TD colSpan={3}>
-              <TextField className="width-100" />
+            <TH colSpan={1} align='right'>산출로직</TH>
+            <TD colSpan={5.01}>
+              <TextField className="width-100" multiline/>
             </TD>
           </TR>
           <TR>
-            <TH colSpan={1}>비고</TH>
-            <TD colSpan={3}>
-              <TextField className="width-100" />
+            <TH colSpan={1} align='right'>비고</TH>
+            <TD colSpan={5.01}>
+              <TextField className="width-100" multiline/>
             </TD>
           </TR>
         </HorizontalTable>
@@ -90,16 +92,24 @@ const Reg = () => {
         <Typography variant="h3">신청 정보</Typography>
         <HorizontalTable>
           <TR>
-            <TH>Feature 신청자</TH>
-            <TD>
+            <TH align='right' colSpan={1}>Feature 신청자</TH>
+            <TD colSpan={2}>
               <Stack gap="SM" className="width-100">
-                <TextField className="width-100" />
-                <Button>검색</Button>
+                <TextField className="width-100" size='MD' />
+                <Button
+                  appearance="Contained"
+                  priority="Normal"
+                  shape="Square"
+                  size="MD"
+                >
+                  <span className="searchIcon"></span>
+                  Search
+                </Button>
               </Stack>
             </TD>
-            <TH>신청부서</TH>
-            <TD>
-              <TextField className="width-100" />
+            <TH align='right' colSpan={1}>신청부서</TH>
+            <TD colSpan={2}>
+              <TextField className="width-100" size='MD' />
             </TD>
           </TR>
         </HorizontalTable>
@@ -107,6 +117,7 @@ const Reg = () => {
 
       <Stack gap="SM" justifyContent="End">
         <Button priority="Primary" appearance="Contained" size="LG">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="currentColor"></path></svg>
           등록
         </Button>
         <Button size="LG" onClick={goToList}>
