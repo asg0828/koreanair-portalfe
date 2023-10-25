4257d485a0cc153e1b549020a5961afdd6658d4c
kjm0810 <kjm980810@gmail.com>
2023-10-25 16:17:52 +0900

    style: layout 병합

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -2,18 +2,31 @@ import { useLocation } from 'react-router-dom';
 import Home from '@/pages/user/Home';
 import Main from '@components/layout/Main';
 import QuickMenu from '@components/layout/QuickMenu';
-import { Stack } from '@components/ui';
+import { Stack,Page } from '@components/ui';
 import './Body.scss';
 
 const Body = () => {
   const location = useLocation();
 
   return (
-    <Stack id="body" direction="Horizontal" alignItems="Start">
-      <QuickMenu />
-
-      {location.pathname === '/' ? <Home /> : <Main />}
+    <>
+    {location.pathname === '/' ? 
+    <Stack id="body" style={{background:'#f8f9fc',paddingBottom:'14px'}} direction="Horizontal" alignItems="Start">
+      <Page style={{padding:'0 20px'}} fixedSize={true}>
+        <Stack alignItems="Start">
+          <QuickMenu />
+          <Home /> 
+        </Stack>
+      </Page>
     </Stack>
-  );
+    : 
+    <Stack id="body" direction="Horizontal" alignItems="Start">
+      <Page style={{padding:'0 20px'}} fixedSize={true}>
+        <Main />
+      </Page>
+    </Stack>
+    }
+    </>
+);
 };
 export default Body;
