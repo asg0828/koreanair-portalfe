a97adfb4fb224bec0318ad2b289324f22791fcca
kjm0810 <pj.jminkim@kalmate.net>
2023-10-27 16:14:24 +0900

    style: header allMenu 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate, NavLink } from 'react-router-dom';
+import { useNavigate, NavLink,Link } from 'react-router-dom';
 import { ReducerType } from '@reducers';
 import { MenuItem } from '@/models/common/Menu';
 import { MenuOutlinedIcon, LogoutOutlinedIcon, KeyboardArrowDownIcon } from '@/assets/icons';
@@ -37,6 +37,13 @@ const Header = () => {
       window.open(`/popup${menu.path}`, '_blank', 'noopener, noreferrer');
     }
   };
+  const [isAllMenuOpen,setIsAllMenuOpen] = useState("close");
+  const allMenuOpen = () => {
+    setIsAllMenuOpen("open");
+  }
+  const allMenuClose = () => {
+    setIsAllMenuOpen("close");
+  }
 
   return (
     <header id="header">
@@ -127,10 +134,58 @@ const Header = () => {
           <Stack justifyContent="End" gap="XL" className="user-info-wrap">
             <Avatar status="01" />
             <LogoutOutlinedIcon color="action" />
-            <MenuOutlinedIcon color="action" />
+            <MenuOutlinedIcon color="action" onClick={allMenuOpen}/>
           </Stack>
         </Stack>
+        
+
       </Page>
+      <div className={"dropDownMenu" +" "+ isAllMenuOpen}>
+        <Page fixedSize={true} style={{ padding: '0 20px' }}>
+          <Stack justifyContent="End" className='width-100' style={{padding:"0 1rem"}}>
+              <button type='button' onClick={allMenuClose}>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="currentColor"></path></svg>
+              </button>
+            </Stack>
+            <div className='dropDownWrap'>
+            {menuList?.map((menu, index: number) => (
+              <div key={`menu-${index}`} className='dropDownItem'>
+                <Link to={menu.path} className='depth1' onClick={allMenuClose}>{menu.name}</Link>
+                {menu.children.length > 0 && (
+                  <div>
+                    {menu.children.map((subMenu, index: number) => (
+                      <div key={`subMenu-${index}`}>
+                        {!subMenu.children[0] || subMenu.children[0].children.length === 0 ? (
+                          <Link to={subMenu.path} className='depth2' onClick={allMenuClose}>
+                            {subMenu.name}
+                          </Link>
+                        ) : (
+                          <div>
+                            <Typography variant="body1">{subMenu.name}</Typography>
+                            {subMenu.children.map((subMenuSecond, index: number) => (
+                              <div
+                                key={`subMenuSecond-${index}`}
+                                className="dropdown-item"
+                              >
+                              <Link to={subMenuSecond.path}  onClick={allMenuClose}>
+                                  <Typography variant="body1" >
+                                    {subMenuSecond.name}
+                                  </Typography>
+                              </Link>
+                              </div> 
+                            ))}
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+            </div>
+          </Page>
+          
+        </div>
     </header>
   );
 };
