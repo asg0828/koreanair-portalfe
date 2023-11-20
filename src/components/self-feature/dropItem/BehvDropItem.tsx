b62120fe6a3e3702bf58dee6b67e455578bef8f4
김태훈A <pj.thoonkim@kalmate.net>
2023-11-20 10:56:57 +0900

    fix: self-feature 속성/행동 명칭 수정

--- src/components/self-feature/dropItem/BehvDropItem.tsx
+++ src/components/self-feature/dropItem/BehvDropItem.tsx
@@ -348,7 +348,7 @@ const BehvDropItem = ({
                 >
                     <Typography variant="h6" style={{color:"inherit"}}>T{itemIdx + 1}</Typography>
                     <div className="dragItemLocation">
-                        행동
+                        BaseFact
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.29498 16.59L12.875 12L8.29498 7.41L9.70498 6L15.705 12L9.70498 18L8.29498 16.59Z" fill="currentColor"></path></svg>
                     </div>
                     <Typography variant="body2" style={{color:"inherit"}}>{targetItem.tableName}</Typography>
