9e607a6988942fbe30f6f6face0f55ffa09738fd
곽승혁 <p.shkwak@kalmate.net>
2023-12-01 16:29:33 +0900

    chore: 구매기여도 총 건수 표시, 국제선 컬럼명 수정

--- src/pages/user/structured-report/purchase-contributors/index.tsx
+++ src/pages/user/structured-report/purchase-contributors/index.tsx
@@ -148,6 +148,7 @@ const List = () => {
                 rows={rows}
                 enableSort={true}
                 clickable={true}
+                page={page}
                 showPageSizeSelect={false}
                 showPagination={false}
                 page={page}
