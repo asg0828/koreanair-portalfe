b1586cb238fdff49c4acc9137d7015e3100b7051
김태훈A <pj.thoonkim@kalmate.net>
2023-12-14 17:35:40 +0900

    fix: self-feature 결재 요청 목록 날짜 및 상태 수정

--- src/pages/user/self-feature-submission/data.tsx
+++ src/pages/user/self-feature-submission/data.tsx
@@ -48,7 +48,7 @@ export const submissionGroupCode: SubmissionGroupCode = {
 
 export const initFeatureSubmissionSearchProps: FeatureSubmissionSearchProps = {
     type: FeatureType.CUST,
-    status: '',
+    submissionStatus: '',
     referenceNo: '',
     submissionNo: '',
     requester: '',
